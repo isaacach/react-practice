1d03c9006a253ff9875e90718fa3fcf16cfa5402
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-07 12:33:13 -0500

    added button and prop types

--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -1,7 +1,29 @@
-/* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
+
+export default function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
+
+  Button.propTypes = {
+    children: PropTypes.string.isRequired,
+    primary: PropTypes.bool,
+    secondary: PropTypes.bool,
+    success: PropTypes.bool,
+    warning: PropTypes.bool,
+    danger: PropTypes.bool,
+    outline: PropTypes.bool,
+    rounded: PropTypes.bool,
+    checkVariationValidity: ({ primary, secondary, success, warning, danger }) => {
+      const count = Number(!!primary) + Number(!!secondary) + Number(!!success) + Number(!!warning) + Number(!!danger);
+      if (count > 1) {
+        return new Error(
+          `Invalid prop, you can only use one variation at a time.`
+        );
+      }
+    }
+  }
+
+  
 
 
-export default function Button({ children }) {
   return (
     <button>{children}</button>
   )
