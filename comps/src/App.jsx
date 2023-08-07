9e06696c6f710fa36c0748ff2f784786598e077d
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-07 12:10:26 -0500

    first commit of comps app

---
+++ comps/src/App.jsx
@@ -0,0 +1,23 @@
+import Button from "./Button";
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <Button>Click me!</Button>
+      </div>
+      <div>
+        <Button>Click here!</Button>
+      </div>
+      <div>
+        <Button>button</Button>
+      </div>
+      <div>
+        <Button>button2</Button>
+      </div>
+      <div>
+        <Button>button3</Button>
+      </div>
+    </div>
+  );
+}
