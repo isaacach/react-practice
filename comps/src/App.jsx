63d3e7c045b51a4dc4b41a2762e201a5ef9fcb3b
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-07 13:33:41 -0500

    added event handler handling

--- comps/src/App.jsx
+++ comps/src/App.jsx
@@ -1,16 +1,31 @@
 import Button from "./Button";
+import { HiOutlineCode, HiOutlineMoon, HiOutlineSparkles } from "react-icons/hi"; 
 
 export default function App() {
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
+
   return (
     <div>
       <div>
-        <Button primary rounded>Click me!</Button>
+        <Button primary rounded className='mb-5' onClick={handleClick}>
+          <HiOutlineCode />
+          Click me!
+          </Button>
       </div>
       <div>
-        <Button secondary outline>Click here!</Button>
+        <Button secondary outline>
+          <HiOutlineMoon />
+          Click here!
+          </Button>
       </div>
       <div>
-        <Button success rounded outline>button</Button>
+        <Button success rounded outline>
+          <HiOutlineSparkles />
+          button
+          </Button>
       </div>
       <div>
         <Button warning>button2</Button>
--- comps/src/Button.jsx
+++ comps/src/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import className from 'classnames';
 
-export default function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
+export default function Button({ children, primary, secondary, success, warning, danger, outline, rounded, ...rest }) {
 
   Button.propTypes = {
     children: PropTypes.string.isRequired,
@@ -22,20 +22,17 @@ export default function Button({ children, primary, secondary, success, warning,
     }
   }
 
-  const classes = className({
+  const classes = className({'flex items-center gap-1 px-3, py-2 border-2': true,
     'bg-blue-500 border-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4': primary,
     'bg-gray-500 border-gray-700 hover:bg-gray-700 text-white font-bold py-2 px-4': secondary,
     'bg-green-500 border-green-700 hover:bg-green-700 text-white font-bold py-2 px-4': success,
     'bg-yellow-500 border-yellow-700 hover:bg-yellow-700 text-white font-bold py-2 px-4': warning,
     'bg-red-500 border-red-700 hover:bg-red-700 text-white font-bold py-2 px-4': danger,
-    'border-2': outline,
+    'bg-white text-inherit': outline,
     'rounded-full': rounded
   });
 
-
-
-
   return (
-    <button className={classes}>{children}</button>
+    <button {...rest} className={classes}>{children}</button>
   )
 }
