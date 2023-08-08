5518caeb32883ec4663cbb40124490d5e26e74fc
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-08 16:42:28 -0500

    added navigation context

---
+++ comps/src/context/navigation.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+
+const NavigationContext = createContext();
+
+function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    }
+  }, []);
+
+  return (
+    <NavigationContext.Provider value={{}}>
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export default NavigationContext;
--- comps/src/main.jsx
+++ comps/src/main.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { NavigationProvider } from './context/navigation.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
 )
