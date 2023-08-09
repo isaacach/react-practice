70c96b1de9be4f76eb8ecc40f9113d1f0891fbda
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-09 16:13:06 -0500

    refactored to allow any table input

--- comps/src/components/Table.jsx
+++ comps/src/components/Table.jsx
@@ -1,15 +1,27 @@
 /* eslint-disable react/prop-types */
 
-export default function Table({ data }) {
-  const renderedData = data.map((item, index) => {
-    return (
-      <tr key={index} className="border-b-2">
-        <td className="p-3 text-center">{item.name}</td>
-        <td className="p-3 flex w-full justify-center">
-          <div className={`w-8 h-8 rounded-full ${item.color}`}></div>
+export default function Table({ data, config }) {
+  const renderedData = data.map((fruit) => {
+    const renderedCells = config.map((col) => {
+      return (
+        <td key={col.label} className="p-3">
+          {col.render(fruit)}
         </td>
-        <td className="p-3 text-center">{item.score}</td>
+      );
+    });
+
+    return (
+      <tr key={fruit.name} className="border-b-2 border-gray-500">
+        {renderedCells}
       </tr>
+    );
+  });
+
+  const renderedHeaders = config.map((col, index) => {
+    return (
+      <th key={index} className="px-4 py-2">
+        {col.label}
+      </th>
     );
   });
 
@@ -17,11 +29,7 @@ export default function Table({ data }) {
     <div>
       <table className="table-auto border-spacing-2 w-96">
         <thead className="bg-gray-200">
-          <tr className="border-b-2 border-gray-500">
-            <th className="px-4 py-2">Fruit</th>
-            <th className="px-4 py-2">Color</th>
-            <th className="px-4 py-2">Score</th>
-          </tr>
+          <tr className="border-b-2 border-gray-500">{renderedHeaders}</tr>
         </thead>
         <tbody>{renderedData}</tbody>
       </table>
--- comps/src/pages/TablePage.jsx
+++ comps/src/pages/TablePage.jsx
@@ -1,7 +1,7 @@
 import Table from "../components/Table";
 
 export default function TablePage() {
-  const data = [
+  const fruit = [
     { name: 'Orange', color: 'bg-orange-500', score: 5 },
     { name: 'Apple', color: 'bg-red-500', score: 3 },
     { name: 'Banana', color: 'bg-yellow-500', score: 4 },
@@ -9,9 +9,27 @@ export default function TablePage() {
     { name: 'Mango', color: 'bg-yellow-500', score: 1 }
   ];
 
+  const config = [
+    {
+      label: 'fruit',
+      render: (fruit) => fruit.name
+    },
+    {
+      label: 'color',
+      render: (fruit) => <div className={`w-8 h-8 rounded-full ${fruit.color}`}></div>
+    },
+    {
+      label: 'score',
+      render: (fruit) => fruit.score,
+      sort: (a, b) => {
+        
+      }
+    }
+  ]
+
   return (
     <div>
-      <Table data={data}/>
+      <Table data={fruit} config={config}/>
     </div>
   )
 
