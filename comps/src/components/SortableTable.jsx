79327a18d6033d8454539fed258c4b9fc663da38
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-10 14:39:23 -0500

    added sorting func to table

--- comps/src/components/SortableTable.jsx
+++ comps/src/components/SortableTable.jsx
@@ -1,8 +1,70 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 import Table from "./Table";
 
 export default function SortableTable(props) {
-  const { config } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
+
+  const getIcons = (label, sortBy, sortOrder) => {
+    if (label !== sortBy) {
+      return (
+        <div>
+          <GoTriangleUp />
+          <GoTriangleDown />
+        </div>
+      );
+    }
+
+    if (sortOrder === null) {
+      return (
+        <div>
+          <GoTriangleUp />
+          <GoTriangleDown />
+        </div>
+      );
+    } else if (sortOrder === "asc") {
+      <div>
+        <GoTriangleUp />
+      </div>;
+    } else if (sortOrder === "desc") {
+      <div>
+        <GoTriangleDown />
+      </div>;
+    }
+  };
+
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
+
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((col) => col.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
 
   const updatedConfig = config.map((col) => {
     if (!col.sortValue) {
@@ -11,9 +73,16 @@ export default function SortableTable(props) {
 
     return {
       ...col,
-      header: () => <th>{col.label} is sortable</th>
+      header: () => (
+        <th onClick={() => handleClick(col.label)}>
+          <div className="flex items-center">
+            {getIcons(col.label, sortBy, sortOrder)}
+            {col.label}
+          </div>
+        </th>
+      ),
     };
   });
 
-  return <Table {...props} config={updatedConfig} />;
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
