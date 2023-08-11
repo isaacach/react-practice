812440763f2a17c7aee9af3a3211165af08d0b7f
isaacach <124728947+isaacach@users.noreply.github.com>
2023-08-11 15:14:34 -0500

    added useSort hook for sortable table

--- comps/src/components/SortableTable.jsx
+++ comps/src/components/SortableTable.jsx
@@ -1,31 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
-import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 import Table from "./Table";
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+import useSort from "../hooks/use-sort";
 
 export default function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
-
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { setSortColumn, sortedData, sortBy, sortOrder } = useSort(config, data);
 
   const updatedConfig = config.map((col) => {
     if (!col.sortValue) {
@@ -36,7 +16,7 @@ export default function SortableTable(props) {
       ...col,
       header: () => (
         <th
-          onClick={() => handleClick(col.label)}
+          onClick={() => setSortColumn(col.label)}
           className="cursor-pointer hover:bg-blue-100"
         >
           <div className="flex items-center justify-center">
@@ -48,53 +28,40 @@ export default function SortableTable(props) {
     };
   });
 
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((col) => col.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
 
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   return <Table {...props} data={sortedData} config={updatedConfig} />;
-}
-function getIcons(label, sortBy, sortOrder) {
-  if (label !== sortBy) {
-    return (
-      <div>
-        <GoTriangleUp />
-        <GoTriangleDown />
-      </div>
-    );
-  }
 
-  if (sortOrder === null) {
-    return (
-      <div>
-        <GoTriangleUp />
-        <GoTriangleDown />
-      </div>
-    );
-  } else if (sortOrder === "asc") {
-    return (
-      <div>
-        <GoTriangleUp />
-      </div>
-    );
-  } else if (sortOrder === "desc") {
-    return (
-      <div>
-        <GoTriangleDown />
-      </div>
-    );
+  function getIcons(label, sortBy, sortOrder) {
+    if (label !== sortBy) {
+      return (
+        <div>
+          <GoTriangleUp />
+          <GoTriangleDown />
+        </div>
+      );
+    }
+  
+    if (sortOrder === null) {
+      return (
+        <div>
+          <GoTriangleUp />
+          <GoTriangleDown />
+        </div>
+      );
+    } else if (sortOrder === "asc") {
+      return (
+        <div>
+          <GoTriangleUp />
+        </div>
+      );
+    } else if (sortOrder === "desc") {
+      return (
+        <div>
+          <GoTriangleDown />
+        </div>
+      );
+    }
   }
 }
+
